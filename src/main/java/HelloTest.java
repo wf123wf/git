006b6d2393018cc496e1f6b1de4c92702c2a9ab4
wf <wf@atguigu.com>
2021-11-30 18:55:21 +0800

    hot-fix commit

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -2,6 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello word");
+        System.out.println("hot-fix");
     }
 }
 
