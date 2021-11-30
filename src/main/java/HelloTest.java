2d92cfaf593ab7b2cbed28d9c281d1866b0718d7
wf <wf@atguigu.com>
2021-11-30 18:46:16 +0800

    first commit

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -1,6 +1,6 @@
-public class Hello {
+public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world 1");
     }
 }
+
